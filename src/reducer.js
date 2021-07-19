6a20f2a7d4b001c0d251b15f7e1ae47385b9aa00
Yisrael Gurkow <yisrelg3@gmail.com>
2021-07-19 09:12:59 -0400

    commit tree 0bcd04359094f95f8c74cf586c3680703d0281b4

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,30 @@
 // add code snippets from README
+let state //= { count: 0 };
+
+function reducer(state= { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+} 
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render()
+  } 
+
+  function render() {
+    let container = document.getElementById("container");
+    container.textContent = state.count;
+  }
+
+  let button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  dispatch({ type: "counter/increment" });
+}); 
+  
+dispatch({type: '@@init'})
+  
